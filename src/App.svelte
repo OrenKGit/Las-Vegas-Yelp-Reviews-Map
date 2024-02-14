e00effca04d6dff654ddf3cd72e3e05d37e0b857
Oren Kaplan <orenkaplan@Orens-Air.lan>
2024-02-14 11:44:01 -0800

    not much

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script>
-    mapboxgl.accessToken = "pk.eyJ1Ijoib2thcGxhbiIsImEiOiJjbHNqbml1OGwycHNqMmxya2J4a3Y0eWVxIn0.HrTTKiUlHO55yvSXHoG4SA";
+	mapboxgl.accessToken = "pk.eyJ1IjoiZXRoYW5saW4xNyIsImEiOiJjbHNqaXJyejIyc3d4MmtwZGV5NmQ5cXJsIn0.LhWgJiTh_1Wj3BHkmZWqSg";
 	const map = new mapboxgl.Map({
 		container: "map",
-		style: "mapbox://styles/mapbox/dark-v11", 
+		style: "mapbox://styles/mapbox/light-v11", 
 		center: [-71.0942, 42.3601], 
 		zoom: 13, // starting zoom level
-		minZoom: 12,
+		minZoom: 2,
 		maxZoom: 15,
 	});
     map.on("load", () => {
@@ -18,11 +18,13 @@
 			type: "line",
 			source: "boston_route",
 			paint: {
-				"line-color": "#c4b0e5",
+				"line-color": "#EEB5B0",
 				"line-width": 3,
 			},
 		});
-        map.addSource("cambridge_route", {
+	});
+    map.on("load", () => {
+		map.addSource("cambridge_route", {
 			type: "geojson",
 			data: "https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson",
 		});
@@ -31,35 +33,35 @@
 			type: "line",
 			source: "cambridge_route",
 			paint: {
-				"line-color": "#c4b0e5",
+				"line-color": "#00E5B0",
 				"line-width": 3,
 			},
 		});
 	});
 
-    map.on("viewreset", position_station_markers);
+	map.on("viewreset", position_station_markers);
 	map.on("move", position_station_markers);
 	map.on("moveend", position_station_markers);
 
-    function create_station_markers(station_data) {
+	function create_station_markers(station_data) {
 		station_markers = marker_container
 			.selectAll("circle")
 			.data(station_data)
 			.enter()
 			.append("circle")
 			.attr("r", 5)
-			.style("fill", 'rgb(255,255,255)') /*#641bde*/
-			.attr("stroke", 'rgb(255,255,255)') //#5f00fc
+			.style("fill", "#808080")
+			.attr("stroke", "#808080")
 			.attr("stroke-width", 1)
 			.attr("fill-opacity", 0.4)
 			.attr("name", function (d) {
 				return d["name"];
 			});
 
-        position_station_markers();
+            position_station_markers();
 	}
 
-    function position_station_markers() {
+	function position_station_markers() {
 		station_markers
 			.attr("cx", function (d) {
 				return project(d).x;
@@ -68,19 +70,32 @@
 				return project(d).y;
 			});
 	}
-
-    function update_station_markers() {
-		station_markers
-            .transition()
-			.duration(1000)
-			.attr("r", function (d) {
-				let trafficVolume =
-					arrivals[d["station_id"]] + departures[d["station_id"]];
-				return scaleRadiusTrafficVolume(trafficVolume);
-			})
-			.style("fill", function (d) {return circolor(arrival_minus_departures[d['station_id']])}) /*#641bde*/
-			.attr("stroke", function (d) {return circolor(arrival_minus_departures[d['station_id']])})
+	function project(d) {
+		return map.project(new mapboxgl.LngLat(+d.lon, +d.lat));
 	}
+
+	function update_station_markers() {
+        station_markers
+            .transition()
+            .duration(1000)
+            .attr("r", function (d) {
+                let trafficVolume = arrivals[d["station_id"]] + departures[d["station_id"]];
+                return scaleRadiusTrafficVolume(trafficVolume);
+            })
+            .style("fill", function (d) {
+                let arrivalRatio = arrivals[d["station_id"]] / (arrivals[d["station_id"]] + departures[d["station_id"]]);
+                return colorScale(arrivalRatio);
+            });
+    }
+
+    // Define a color scale
+    const colorScale = d3.scaleLinear()
+        .domain([0, 0.5, 1]) // Assuming ratio varies from 0 to 1
+        .range(["red", 'white', 'green']); // Choose your colors
+
+    // Adjust the color scale range based on your preferences
+
+    
 
 	function scaleRadiusTrafficVolume(traffic, maxTraffic = 500) {
 		const scaleRadius = d3.scaleSqrt()
@@ -88,24 +103,7 @@
 			.range([0, 3, 20]);
 		return scaleRadius(traffic);
 	}
-
-	function project(d) {
-		return map.project(new mapboxgl.LngLat(+d.lon, +d.lat));
-	}
-
-    function tallyTrips(trips_data) {
-		arrivals.fill(0);
-		departures.fill(0);
-		arrival_minus_departures.fill(0);
-		for (let i = 0; i < trips_data.length; ++i) {
-			arrivals[trips_data[i]["end station id"]]++;
-			departures[trips_data[i]["start station id"]]++;
-			arrival_minus_departures[trips_data[i]["end station id"]]++;
-			arrival_minus_departures[trips_data[i]["start station id"]]--;
-		}
-	}
-
-    function filterTrips(sliderTime) {
+	function filterTrips(sliderTime) {
 		let value = sliderTimeScale(sliderTime);
 		let filterWindowHours = 0;
 		let filterWindowMinutes = 120;
@@ -132,26 +130,51 @@
 		.domain([0, 1440])
 		.range([new Date("2015-12-01 00:00"), new Date("2015-12-02 00:00")]);
 
-    // stations data
+
+	function tallyTrips(trips_data) {
+		arrivals.fill(0);
+		departures.fill(0);
+		for (let i = 0; i < trips_data.length; ++i) {
+			arrivals[trips_data[i]["end station id"]]++;
+			departures[trips_data[i]["start station id"]]++;
+		}
+	}
+
+
     let stationsFile = "https://raw.githubusercontent.com/dsc-courses/dsc106-wi24/gh-pages/resources/data/lab6_station_info.json";
-	let station_data = [];
+	let tripFile = "https://raw.githubusercontent.com/dsc-courses/dsc106-wi24/gh-pages/resources/data/lab6_bluebikes_2020.csv";
+    let station_data = [];
 	let station_markers;
-    // trips data
-    let tripFile = "https://raw.githubusercontent.com/dsc-courses/dsc106-wi24/gh-pages/resources/data/lab6_bluebikes_2020.csv";
-    let trip_data = [];
-    let filtered_trip_data = [];
-    let arrivals = new Array(600).fill(0);
+	let trip_data = [];
+	let arrivals = new Array(600).fill(0);
 	let departures = new Array(600).fill(0);
-	let arrival_minus_departures = new Array(600).fill(0);
-	const circolor = d3.scaleLinear().range(["#cc0000", "#6600cc","#0300cc"]);;
-    // slider
+	let filtered_trip_data = [];
+
     let slider_time = 720;
 	let slider_label = "";
+
 
 	fetch(stationsFile)
 		.then((response) => response.json())
 		.then((d) => (station_data = d.data.stations))
-        .then((d) => create_station_markers(d));
+		.then((d) => create_station_markers(d));
+    
+	d3.csv(tripFile).then(function (d) {
+		trip_data = d;
+		// console.log(trip_data);
+	});
+$: {
+	if (trip_data.length !== 0) {
+			filtered_trip_data = filterTrips(slider_time);
+			tallyTrips(filtered_trip_data);
+			update_station_markers();
+		}
+	slider_label = sliderTimeScale(slider_time).toLocaleTimeString([], {
+		hour: "numeric",
+		minute: "2-digit",
+	});
+}
+
 
     const marker_container = d3
 		.select(map.getCanvasContainer() )
@@ -161,28 +184,11 @@
 		.style("position", "absolute")
 		.style("z-index", 2);
 
-    d3.csv(tripFile).then(function (d) {
-		trip_data = d;
-		console.log(trip_data);
-	});
-
-    $: {
-        if (trip_data.length !== 0) {
-            filtered_trip_data = filterTrips(slider_time);
-			tallyTrips(filtered_trip_data);
-            update_station_markers();
-	    }
-        slider_label = sliderTimeScale(slider_time).toLocaleTimeString([], {
-		hour: "numeric",
-		minute: "2-digit",
-	    });
-    }
 
 </script>
 
 <main>
-    <div class="overlay">
-		<!-- svelte-ignore a11y-label-has-associated-control -->
+	<div class="overlay">
 		<label>{slider_label}</label>
 		<input
 			id="slider"
@@ -191,5 +197,5 @@
 			max="1440"
 			bind:value={slider_time}
 		/>
-	</div>
+    </div>
 </main>
