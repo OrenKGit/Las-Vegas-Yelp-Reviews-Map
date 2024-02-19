101fb082618ea5ac11ffb93f7ffe8f6fcc7eb37b
m-que <mque@ucsd.edu>
2024-02-18 18:48:02 -0800

    filtered data, some code for points

--- src/App.svelte
+++ src/App.svelte
@@ -16,7 +16,7 @@ onMount(async () => {
     console.log(tempData);
 
     mapboxgl.accessToken =
-    'pk.eyJ1Ijoicm9iaW5rb2hycyIsImEiOiJjanU5am95bm4xZnZ6NDNrOTRyYTYwdzJzIn0.iMFQgQIlhz36wB3819Xftw';
+    'pk.eyJ1IjoibXF1ZSIsImEiOiJjbHNtMm51bGcwaWhqMmlyeXZocjNhdXRiIn0.FeNJ2Al_w7at2zuWD-P_qA';
 
     // create map
     /*map = new mapboxgl.Map({
@@ -47,28 +47,76 @@ onMount(async () => {
         const marker = new mapboxgl.Marker({ color: '#008000' }); // Create a green marker
 
         geocoder.on('result', async (event) => {
-        // When the geocoder returns a result
-        const point = event.result.center; // Capture the result coordinates
+            // When the geocoder returns a result
+            const point = event.result.center; // Capture the result coordinates
 
-        marker.setLngLat(point).addTo(map); // Add the marker to the map at the result coordinates
+            marker.setLngLat(point).addTo(map); // Add the marker to the map at the result coordinates
 
-        // not working in console ?
-        const tileset = 'mque.90buekyy' // id for mapbox tileset (data converted to mapbox format)
-        const radius = 1609; // one mile
-        const limit = 50; // max amount of results to return
+            // access token and id need to be from same account
+            const tileset = 'mque.asmw1dy4' // id for mapbox tileset (data converted to mapbox format)
+            const radius = 1609; // one mile
+            const limit = 50; // max amount of results to return
+            
+            marker.setLngLat(point).addTo(map); // Add the marker to the map at the result coordinates
+            const query = await fetch(
+                `https://api.mapbox.com/v4/${tileset}/tilequery/${point[0]},${point[1]}.json?radius=${radius}&limit=${limit}&access_token=${mapboxgl.accessToken}`,
+                { method: 'GET' }
+            );
+            const json = await query.json();
+            map.getSource('tilequery').setData(json);
+        });
+
+        map.addSource('tilequery', {
+            // Add a new source to the map style: https://docs.mapbox.com/mapbox-gl-js/api/#map#addsource
+            type: 'geojson',
+            data: {
+                type: 'FeatureCollection',
+                features: []
+            }
+        });
+
+        map.addLayer({
+            id: 'tilequery-points',
+            type: 'circle',
+            source: 'tilequery', // Set the layer source
+            paint: {
+                'circle-stroke-color': 'white',
+                'circle-stroke-width': {
+                stops: [
+                    [0, 0.1],
+                    [18, 3]
+                ],
+                base: 5
+                },
+                'circle-radius': {
+                stops: [
+                    [12, 5],
+                    [22, 180]
+                ],
+                base: 5
+                },
+                'circle-color': [
+                // Specify the color each circle should be
+                'match',
+                ['get', 'stars'], // getting stars and coloring them
+                5,
+                '#008000',
+                4,
+                '#9ACD32',
+                3,
+                '#FF8C00',
+                2,
+                '#FF0000',
+                1,
+                '#FF0000'
+                ]
+            }
+            });
         
-        marker.setLngLat(point).addTo(map); // Add the marker to the map at the result coordinates
-        const query = await fetch(
-            `https://api.mapbox.com/v4/${tileset}/tilequery/${point[0]},${point[1]}.json?radius=${radius}&limit=${limit}&access_token=${mapboxgl.accessToken}`,
-            { method: 'GET' }
-        );
-        const json = await query.json();
         console.log(json);
         });
         });
-    
 
-});
 
 console.log(tempData)
 
